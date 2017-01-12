0436da5e2138310bccf9064cf8ebd3eeaf9b0c2a
nuriacserra <nucose@hotmail.es>
2017-01-12 09:42:29 +0100

    repte 4
    part 3 i 4

--- Repte__4_Control_Structures_part4/Repte__4_Control_Structures_part4.ino
+++ Repte__4_Control_Structures_part4/Repte__4_Control_Structures_part4.ino
@@ -1,9 +1,55 @@
-void setup() {
-  // put your setup code here, to run once:
+/********************************************************************************************************
+ **
+ **                      Repte 4 Control Structures 
+ **                            Part 4 
+ **                            
+ **   Nom: Núria Cordero Serra               Date: 11/01/2017
+ *********************************************************************************************************/
+//****************************Include**********************************
 
+
+//****************************Variables********************************
+
+float qualEnergy = 15;
+
+//****************************Setup************************************
+
+void setup ()
+{
+  Serial.begin(9600);
+  
+  if (qualEnergy < 3.5)
+  {
+    Serial.print("A");
+  }
+  else if (qualEnergy >= 3.5 & qualEnergy < 6.5)
+  {
+    Serial.print("B");
+  }
+  else if (qualEnergy >= 6.5 & qualEnergy <11.1)
+  {
+    Serial.print("C");
+  }
+  else if (qualEnergy >= 11.1 & qualEnergy <17.7)
+  {
+    Serial.print("D");
+  }
+  else if (qualEnergy >= 17.7 & qualEnergy < 38.2 )
+  {
+    Serial.print("E");
+  }
+  else if (qualEnergy >= 38.2 & qualEnergy <43.2)
+  {
+    Serial.print("F");
+  }
+  else
+  {
+    Serial.print("G");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+//***************************Loop**************************************
+ void loop ()
+ {
+ }
+//***************************Funcions**********************************
